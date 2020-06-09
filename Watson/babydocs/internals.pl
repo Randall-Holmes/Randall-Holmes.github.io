af061c3e88103cea13ab5eb7763edf69fbd58d87
Randall Holmes <rholmes@boisestate.edu>
2020-06-08 20:09:58 -0600

    Add files via upload

---
+++ Watson/babydocs/internals.pl
@@ -0,0 +1,114 @@
+# LaTeX2HTML 98.1p1 release (March 2nd, 1998)
+# Associate internals original text with physical files.
+
+
+$key = q/cite_mathematica/;
+$ref_files{$key} = "$dir".q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_eft/;
+$ref_files{$key} = "$dir".q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Camilleri87/;
+$ref_files{$key} = "$dir".q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Church40/;
+$ref_files{$key} = "$dir".q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_automath/;
+$ref_files{$key} = "$dir".q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_finalreport/;
+$ref_files{$key} = "$dir".q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_nfu/;
+$ref_files{$key} = "$dir".q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_trc/;
+$ref_files{$key} = "$dir".q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_cohen/;
+$ref_files{$key} = "$dir".q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_otter/;
+$ref_files{$key} = "$dir".q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_maple/;
+$ref_files{$key} = "$dir".q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_stratified/;
+$ref_files{$key} = "$dir".q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_rrl/;
+$ref_files{$key} = "$dir".q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_textbook/;
+$ref_files{$key} = "$dir".q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_boyer-moore/;
+$ref_files{$key} = "$dir".q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_knuth-bendix/;
+$ref_files{$key} = "$dir".q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_nuprl/;
+$ref_files{$key} = "$dir".q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_capsl/;
+$ref_files{$key} = "$dir".q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_debruijn/;
+$ref_files{$key} = "$dir".q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_nf/;
+$ref_files{$key} = "$dir".q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_lcf/;
+$ref_files{$key} = "$dir".q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Gordon87/;
+$ref_files{$key} = "$dir".q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_svo/;
+$ref_files{$key} = "$dir".q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_imps/;
+$ref_files{$key} = "$dir".q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_ml/;
+$ref_files{$key} = "$dir".q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_gries/;
+$ref_files{$key} = "$dir".q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_jech/;
+$ref_files{$key} = "$dir".q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
---
+++ Watson/babydocs/labels.pl
@@ -0,0 +1,114 @@
+# LaTeX2HTML 98.1p1 release (March 2nd, 1998)
+# Associate labels original text with physical files.
+
+
+$key = q/cite_mathematica/;
+$external_labels{$key} = "$URL/" . q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_eft/;
+$external_labels{$key} = "$URL/" . q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Camilleri87/;
+$external_labels{$key} = "$URL/" . q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Church40/;
+$external_labels{$key} = "$URL/" . q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_automath/;
+$external_labels{$key} = "$URL/" . q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_finalreport/;
+$external_labels{$key} = "$URL/" . q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_nfu/;
+$external_labels{$key} = "$URL/" . q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_trc/;
+$external_labels{$key} = "$URL/" . q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_cohen/;
+$external_labels{$key} = "$URL/" . q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_otter/;
+$external_labels{$key} = "$URL/" . q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_maple/;
+$external_labels{$key} = "$URL/" . q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_stratified/;
+$external_labels{$key} = "$URL/" . q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_rrl/;
+$external_labels{$key} = "$URL/" . q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_textbook/;
+$external_labels{$key} = "$URL/" . q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_boyer-moore/;
+$external_labels{$key} = "$URL/" . q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_knuth-bendix/;
+$external_labels{$key} = "$URL/" . q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_nuprl/;
+$external_labels{$key} = "$URL/" . q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_capsl/;
+$external_labels{$key} = "$URL/" . q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_debruijn/;
+$external_labels{$key} = "$URL/" . q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_nf/;
+$external_labels{$key} = "$URL/" . q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_lcf/;
+$external_labels{$key} = "$URL/" . q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Gordon87/;
+$external_labels{$key} = "$URL/" . q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_svo/;
+$external_labels{$key} = "$URL/" . q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_imps/;
+$external_labels{$key} = "$URL/" . q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_ml/;
+$external_labels{$key} = "$URL/" . q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_gries/;
+$external_labels{$key} = "$URL/" . q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_jech/;
+$external_labels{$key} = "$URL/" . q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
