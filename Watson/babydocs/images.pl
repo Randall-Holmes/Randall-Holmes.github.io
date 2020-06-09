64587aeabe1dd5500e6f86ce3f0e58989cef4c52
Randall Holmes <rholmes@boisestate.edu>
2020-06-08 20:07:56 -0600

    Add files via upload

---
+++ Watson/babydocs/images.pl
@@ -0,0 +1,286 @@
+# LaTeX2HTML 98.1p1 release (March 2nd, 1998)
+# Associate images original text with physical files.
+
+
+$key = q/{inline}alpharightarrowalpha{inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="49" HEIGHT="13" ALIGN="BOTTOM" BORDER="0"
+ SRC="img26.gif"
+ ALT="$\alpha
+\rightarrow \alpha$">|; 
+
+$key = q/{inline}(existsx.T)=T[{inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="87" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="img16.gif"
+ ALT="$(\exists x.T) = T[$">|; 
+
+$key = q/{inline}lambdax{inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="22" HEIGHT="14" ALIGN="BOTTOM" BORDER="0"
+ SRC="img41.gif"
+ ALT="$\lambda x$">|; 
+
+$key = q/{inline}(lambdax.x(x)){inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="69" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="img18.gif"
+ ALT="$(\lambda x.x(x))$">|; 
+
+$key = q/{inline}(alphatimesbeta)rightarrowbeta{inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="90" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="img24.gif"
+ ALT="$(\alpha\times\beta)\rightarrow \beta$">|; 
+
+$key = q/{inline}(lambdax.T)(A)=T[Aslashx]{inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="145" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="img10.gif"
+ ALT="$(\lambda x.T)(A) = T[A/x]$">|; 
+
+$key = q/{inline}tau(tau(x))=tau(x){inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="106" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="img37.gif"
+ ALT="$\tau(\tau(x)) = \tau(x)$">|; 
+
+$key = q/{inline}(lambdax.bftrue){inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="72" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="img35.gif"
+ ALT="$(\lambda x.{\bf true})$">|; 
+
+$key = q/{inline}forall{inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="12" HEIGHT="15" ALIGN="BOTTOM" BORDER="0"
+ SRC="img12.gif"
+ ALT="$\forall$">|; 
+
+$key = q/{inline}(alphatimesbeta)rightarrowgamma{inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="89" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="img32.gif"
+ ALT="$(\alpha \times \beta) \rightarrow \gamma$">|; 
+
+$key = q/{inline}lambda{inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="13" HEIGHT="14" ALIGN="BOTTOM" BORDER="0"
+ SRC="img1.gif"
+ ALT="$\lambda $">|; 
+
+$key = q/{inline}(alphatimesbeta)rightarrowalpha{inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="90" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="img23.gif"
+ ALT="$(\alpha\times\beta)\rightarrow \alpha$">|; 
+
+$key = q/{inline}alphatimesbeta{inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="43" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
+ SRC="img21.gif"
+ ALT="$\alpha \times \beta$">|; 
+
+$key = q/{inline}nrightarrown{inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="47" HEIGHT="13" ALIGN="BOTTOM" BORDER="0"
+ SRC="img27.gif"
+ ALT="$n \rightarrow n$">|; 
+
+$key = q/{inline}ldots{inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="22" HEIGHT="13" ALIGN="BOTTOM" BORDER="0"
+ SRC="img29.gif"
+ ALT="$\ldots$">|; 
+
+$key = q/{inline}alpharightarrow(betarightarrowgamma){inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="94" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="img33.gif"
+ ALT="$\alpha \rightarrow (\beta \rightarrow \gamma)$">|; 
+
+$key = q/{inline}pi_1{inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="20" HEIGHT="28" ALIGN="MIDDLE" BORDER="0"
+ SRC="img3.gif"
+ ALT="$\pi_1$">|; 
+
+$key = q/{inline}sim{inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="39" HEIGHT="50" ALIGN="BOTTOM" BORDER="0"
+ SRC="img2.gif"
+ ALT="$\sim$">|; 
+
+$key = q/{inline}pi_2{inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="20" HEIGHT="28" ALIGN="MIDDLE" BORDER="0"
+ SRC="img4.gif"
+ ALT="$\pi_2$">|; 
+
+$key = q/{inline}tau{inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="12" HEIGHT="13" ALIGN="BOTTOM" BORDER="0"
+ SRC="img36.gif"
+ ALT="$\tau$">|; 
+
+$key = q/{inline}alpharightarrowbeta{inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="48" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
+ SRC="img22.gif"
+ ALT="$\alpha \rightarrow \beta$">|; 
+
+$key = q/{inline}nleql_1{inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="46" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
+ SRC="img44.gif"
+ ALT="$n \leq l_1$">|; 
+
+$key = q/{inline}iota{inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="9" HEIGHT="13" ALIGN="BOTTOM" BORDER="0"
+ SRC="img19.gif"
+ ALT="$\iota$">|; 
+
+$key = q/{inline}(lambdax.U){inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="51" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="img6.gif"
+ ALT="$(\lambda x.U)$">|; 
+
+$key = q/{inline}kappa{inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="12" HEIGHT="13" ALIGN="BOTTOM" BORDER="0"
+ SRC="img38.gif"
+ ALT="$\kappa$">|; 
+
+$key = q/{inline}pi_2(x,y)=y{inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="86" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="img8.gif"
+ ALT="$\pi_2(x,y) = y$">|; 
+
+$key = q/{inline}(lambdax.x){inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="48" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="img42.gif"
+ ALT="$(\lambda x.x)$">|; 
+
+$key = q/{inline}(lambdax.U[xslashT])(T){inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="112" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="img46.gif"
+ ALT="$(\lambda x.U[x/T])(T)$">|; 
+
+$key = q/{inline}(lambdax^alpha.x^alpha){inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="66" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="img25.gif"
+ ALT="$(\lambda
+x^{\alpha}.x^{\alpha})$">|; 
+
+$key = q/{inline}kappa(tau(x))=(lambday.tau(x)){inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="141" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="img39.gif"
+ ALT="$\kappa(\tau(x)) = (\lambda y.\tau(x))$">|; 
+
+$key = q/{inline}beta{inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="13" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
+ SRC="img9.gif"
+ ALT="$\beta$">|; 
+
+$key = q/{inline}(lambdax.T)slashx]{inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="72" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="img17.gif"
+ ALT="$(\lambda x.T)/x]$">|; 
+
+$key = q/{inline}ntimesn{inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="42" HEIGHT="28" ALIGN="MIDDLE" BORDER="0"
+ SRC="img28.gif"
+ ALT="$n \times n$">|; 
+
+$key = q/{inline}exists{inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="12" HEIGHT="14" ALIGN="BOTTOM" BORDER="0"
+ SRC="img15.gif"
+ ALT="$\exists$">|; 
+
+$key = q/{inline}(forallx.T)=((lambdax.T)=(lambdax.bftrue)){inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="219" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="img13.gif"
+ ALT="$(\forall x.T) =
+((\lambda x.T) = (\lambda x.{\bf true}))$">|; 
+
+$key = q/{inline}l_2<nleql_1{inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="79" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
+ SRC="img45.gif"
+ ALT="$l_2 < n \leq l_1$">|; 
+
+$key = q/{inline}alpha{inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="14" HEIGHT="13" ALIGN="BOTTOM" BORDER="0"
+ SRC="img20.gif"
+ ALT="$\alpha$">|; 
+
+$key = q/{inline}(lambdax.P){inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="51" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="img34.gif"
+ ALT="$(\lambda x.P)$">|; 
+
+$key = q/{inline}(lambday.(lambdax.x)){inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="82" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="img43.gif"
+ ALT="$(\lambda y.(\lambda x.x))$">|; 
+
+$key = q/{inline}(lambdax.f(x))=f{inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="100" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="img11.gif"
+ ALT="$(\lambda x. f(x)) = f$">|; 
+
+$key = q/{inline}^wedge{inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="13" HEIGHT="16" ALIGN="BOTTOM" BORDER="0"
+ SRC="img40.gif"
+ ALT="$^{\wedge}$">|; 
+
+$key = q/{inline}(iotarightarrowiota)rightarrowiota{inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="82" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="img30.gif"
+ ALT="$(\iota \rightarrow \iota) \rightarrow \iota$">|; 
+
+$key = q/{inline}iotarightarrow(iotarightarrowiota){inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="82" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="img31.gif"
+ ALT="$\iota \rightarrow
+(\iota \rightarrow \iota)$">|; 
+
+$key = q/{inline}pi_1(x,y)=x{inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="87" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="img7.gif"
+ ALT="$\pi_1(x,y) = x$">|; 
+
+$key = q/{inline}(lambdax.T){inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="50" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="img5.gif"
+ ALT="$(\lambda x.T)$">|; 
+
+$key = q/{inline}forall(lambdax.T){inline}MSF=1.6;AAT;/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="59" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="img14.gif"
+ ALT="$\forall(\lambda x.T)$">|; 
+
+1;
+
